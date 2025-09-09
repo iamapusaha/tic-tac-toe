2a23b6e1f9d8d5feba54cffaef76ee2630b314e0
iamapusaha <apusaha392@gmail.com>
2025-09-09 17:14:17 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-8xl, text-center text-blue-600">Hello tic tac toe</h1>
+    </div>
+  );
+}
+
+export default App;
